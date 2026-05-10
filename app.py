5d0aa67d47dd62304fee8b85cb99a7fc65c0002c
A Naveen Kumar <kumarnaveen99375@gmail.com>
2026-05-10 16:06:08 +0530

    changed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)#object creation
@@ -59,6 +59,23 @@ def master():#view
 
     return render_template("master.html")
 
+
+#API Creation
+@app.route('/api/books')
+def get_books():
+    books=Book.query.all()
+    return jsonify([book.to_dict() for book in books])
+    #data_type=type(books)
+   #@ return data_type
+
+@app.route('/api/books' , methods=['POST',])
+def add_book():
+        data = request.get_json()
+        new_book = Book(id = data['id'], book=data['book'], author=data['author'], price= data['price'])
+        db.session.add(new_book)
+        db.session.commit()
+        return jsonify(new_book.to_dict()), 201
+        
 
 
 if __name__== '__main__':
